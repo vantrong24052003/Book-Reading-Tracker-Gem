49fe1bb8e02053849df97f50e9697b0ac35423d5
Doan vo van trong <trong.doan@tomosia.com>
2025-03-27 15:33:21 +0700

    fix: change bin/book

--- lib/book_reading_tracker_gem/clis/cli.rb
+++ lib/book_reading_tracker_gem/clis/cli.rb
@@ -16,7 +16,7 @@ module BookReadingTrackerGem
     option :isbn, required: false
     option :published_year, required: false, type: :numeric
     def add_book(title)
-      # ruby bin/remind add_book "Ruby Programming" --author "David Heinemeier Hansson" --pages 300 --description "Learn Ruby with Rails" --isbn "978-0134596882" --published_year 2023
+      # ruby bin/book add_book "Ruby Programming" --author "David Heinemeier Hansson" --pages 300 --description "Learn Ruby with Rails" --isbn "978-0134596882" --published_year 2023
       BookService.add_book(
         title,
         options[:author],
@@ -28,7 +28,7 @@ module BookReadingTrackerGem
     end
 
     desc 'remove_book ID', 'Xóa sách khỏi danh sách'
-    # ruby bin/remind remove_book 1
+    # ruby bin/book remove_book 1
     def remove_book(id)
       BookService.remove_book(id)
     end
@@ -36,24 +36,24 @@ module BookReadingTrackerGem
     desc 'progress_book ID --page PAGE', 'Cập nhật tiến độ đọc sách'
     option :page, required: true, type: :numeric
     def progress_book(id)
-      # ruby bin/remind progress_book 2 --page 150
+      # ruby bin/book progress_book 2 --page 150
       BookService.update_progress(id, options[:page])
     end
 
     desc 'list_books', 'Hiển thị danh sách sách'
-    # ruby bin/remind list_books
+    # ruby bin/book list_books
     def list_books
       BookService.list_books
     end
 
     desc 'stats', 'Hiển thị thống kê sách'
-    # ruby bin/remind stats
+    # ruby bin/book stats
     def stats
       BookService.stats
     end
 
     desc 'show_progress ID', 'Xem tiến độ đọc sách'
-    # ruby bin/remind show_progress "Ruby Programming"
+    # ruby bin/book show_progress "Ruby Programming"
     def show_progress(id)
       BookService.show_progress(id)
     end
@@ -62,25 +62,25 @@ module BookReadingTrackerGem
     desc 'add_author NAME [--biography BIOGRAPHY]', 'Thêm tác giả mới'
     option :biography, required: false
     def add_author(name)
-      # ruby bin/remind add_author "J.K. Rowling" --biography "Tác giả của Harry Potter"
+      # ruby bin/book add_author "J.K. Rowling" --biography "Tác giả của Harry Potter"
       AuthorService.add_author(name, options[:biography])
     end
 
     desc 'list_authors', 'Hiển thị danh sách tác giả'
     def list_authors
-      # ruby bin/remind list_authors
+      # ruby bin/book list_authors
       AuthorService.list_authors
     end
 
     # Quản lý danh mục
     desc 'add_category NAME', 'Thêm danh mục mới'
     def add_category(name)
-      # ruby bin/remind add_category "Fiction"
+      # ruby bin/book add_category "Fiction"
       CategoryService.add_category(name)
     end
 
     desc 'list_categories', 'Hiển thị danh sách danh mục'
-    # ruby bin/remind list_categories
+    # ruby bin/book list_categories
     def list_categories
       CategoryService.list_categories
     end
