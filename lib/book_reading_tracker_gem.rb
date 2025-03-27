25ea9c635278dd342bfad39e36e41fdf7b3fb790
Doan vo van trong <trong.doan@tomosia.com>
2025-03-27 13:06:30 +0700

    fix: apply main

--- lib/book_reading_tracker_gem.rb
+++ lib/book_reading_tracker_gem.rb
@@ -1,7 +1,17 @@
 # frozen_string_literal: true
 
 require_relative 'book_reading_tracker_gem/version'
-require_relative '/lib/book_reading_tracker_gem/clis/cli.rb'
+require_relative 'book_reading_tracker_gem/clis/cli'
+require_relative 'book_reading_tracker_gem/services/book_service'
+require_relative 'book_reading_tracker_gem/services/author_service'
+require_relative 'book_reading_tracker_gem/services/category_service'
+require_relative 'book_reading_tracker_gem/models/book'
+require_relative 'book_reading_tracker_gem/models/author'
+require_relative 'book_reading_tracker_gem/models/category'
+require_relative 'book_reading_tracker_gem/models/book_author'
+require_relative 'book_reading_tracker_gem/models/book_category'
+require_relative 'book_reading_tracker_gem/models/reading_progress'
+
 module BookReadingTrackerGem
   class Error < StandardError; end
   
